771dbb7d3c7b09a9ac592f249f1632cc05699b8f
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-04-20 16:19:40 -0300

    Documentando API com Swagger e SpringFox

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -63,7 +63,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	// Acesso a JS, CSS, Imagens...
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		super.configure(web);
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 	
 	/* Convertendo a senha textual em BCrypt
